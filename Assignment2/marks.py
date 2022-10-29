a29d1039e8d163c6c4b6dc0f551515fc9c06cbaa
KajalJadhav710 <kajaljadhav0710@gmail.com>
2022-10-29 21:05:23 +0530

    assignment02

---
+++ Assignment2/marks.py
@@ -0,0 +1,10 @@
+a = int(input("enter the marathi marks :"))
+b = int(input("enter the hindi marks :"))
+c = int(input("enter the english marks :"))
+d = int(input("enter the maths marks :"))
+e = int(input("enter the science marks :"))
+total = a+b+c+d+e
+print(total)
+
+per = total/5
+print("percentage is : ",per)
