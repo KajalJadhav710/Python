6e5d2d9cc52dcc42f2c2c70b56ec50ff3e1ecc46
KajalJadhav710 <kajaljadhav0710@gmail.com>
2022-12-02 18:53:33 +0530

    Assignment13

---
+++ Assignment13/Marks_function.py
@@ -0,0 +1,15 @@
+def mrk():
+
+    x = [88,69,74,58,70]
+    sum = 0
+
+    for i in x :
+        sum = sum + i
+    return sum 
+
+y = mrk()
+print(" marks of sumation is : " , y)
+
+print(mrk())
+per = y/5
+print(" marks of Percentage is : " , per)
