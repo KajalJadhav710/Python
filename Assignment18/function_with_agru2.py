184f5e4f810ba80db8a858bb7f8c96253c0829cc
KajalJadhav710 <kajaljadhav0710@gmail.com>
2023-01-01 14:10:57 +0530

    Assignment18

---
+++ Assignment18/function_with_agru2.py
@@ -0,0 +1,23 @@
+def demo(a, b=0):
+    print(a,b)
+
+def person(name,age=18):
+    print("name is ",name)
+    print("age is ", age)
+
+
+def student_info(name,clg="AIT"):
+    print(name,clg)
+
+    
+demo(23,48)    
+demo(2,7)
+person ("kajal",22)
+person ("tejal")
+
+
+
+
+student_info("Tejal")
+student_info("Bhagyashri","AIT")
+student_info("Gayu","VHV")  
---
+++ Assignment18/function_with_argu.py
@@ -0,0 +1,16 @@
+def add (a,b,c):
+    print("a value is :",a)
+    print("b value is :",b)
+    C = a+b+c
+    return e
+
+def power(a,b):
+    return a**b
+print(power(4,8))
+
+def remainder(a,b):
+    return a%b
+print(remainder(16,81))
+
+
+
