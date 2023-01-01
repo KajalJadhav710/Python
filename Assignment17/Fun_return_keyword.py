b9ac78cdabe3efffc8c8b380fdb85e169244083b
KajalJadhav710 <kajaljadhav0710@gmail.com>
2023-01-01 14:10:22 +0530

    Assignment17

---
+++ Assignment17/Fun_return_keyword.py
@@ -0,0 +1,38 @@
+def sub(x,y):
+    z = x-y
+    return z
+
+print(sub(56,20))
+p = sub(20,40)
+print(p)
+
+print("------")
+
+def add(a,b):
+    c = a+b
+    return c
+
+print(add(40,70))
+d = add(59,89)
+print(d)
+
+print("-------")
+
+def div(R,S):
+    T = R/S
+    return T
+
+print(div(45,5))
+U = div(49,7)
+print(U)
+
+print("------")
+
+def mul(e,f):
+    g = e*f
+    return g
+
+print(mul(3,2))
+k = mul(8,9)
+print(k)
+
